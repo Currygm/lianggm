3d0258bce544695fcebb126e4b802ac4748be6c3
gm <2577924591@qq.com>
2025-07-08 15:14:48 +0800

    update greedy_algorithm01

--- greedy_algorithm1.cpp
+++ greedy_algorithm1.cpp
@@ -13,13 +13,14 @@ public:
         for(int i = 0;i < l;i++){
             maxDay = max(maxDay, events[i][1]);  //记录最大的截止日期
         }
-        sort(events.begin(), events.end());
+        sort(events.begin(), events.end());  //对二维vector排序，默认按元素升序，即先比较第一个元素，以此类推
         priority_queue<int, vector<int>, greater<int>> mq; //最小堆，用于升序保存各个会议的结束日期，即最小的放在最前面
         //遍历每一个日期
         for(int i = 1, j = 0;i <= maxDay;i++){
-            //筛选处开始时间早于i天的会议
+            //i表示日期，j表示会议，已参加过的会议不会重复参加（i，j都只初始化一次）
+            //筛选出开始时间早于i天的会议（已参加过的会议不再放入）
             while(j < l && events[j][0] <= i){
-                mq.emplace(events[j][1]);
+                mq.emplace(events[j][1]); //向优先队列加入新元素
                 j++;
             }
             //将结束时间早于i天的过滤掉
